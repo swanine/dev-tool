f2510359a7f61eac8ff28b4ff13dd9e220f60334
swanine <haohao1500@foxmail.com>
2022-04-30 17:36:29 +0800

    🔧 chore: remove withInstall func

--- packages/rosiui/src/components/button/index.ts
+++ packages/rosiui/src/components/button/index.ts
@@ -4,9 +4,6 @@ import Button from './src/button'
 export * from './src/type'
 
 export const RButton = {
-  title: 'Button 按钮',
-  category: '通用',
-  status: '100%',
   install(app: App): void {
     app.component(Button.name, Button)
   }
