0d1482234c121a6479f15ce8dbf4760a9d97f402
swanine <haohao1500@foxmail.com>
2022-05-02 19:21:45 +0800

    💫 refactor: change button apply mixins

--- packages/rosiui/demo/cases/Button.vue
+++ packages/rosiui/demo/cases/Button.vue
@@ -1,7 +1,14 @@
 <template>
-  <r-button theme="primary" round @click="handleClick('hello, rosi')"
-    >都是</r-button
-  >
+  <div>
+    <r-button theme="primary">都是</r-button>
+    <r-button theme="success">都是</r-button>
+    <r-button theme="warning">都是</r-button>
+    <r-button theme="danger">都是</r-button>
+    <r-button theme="light">都是</r-button>
+    <r-button theme="dark">都是</r-button>
+    <r-button theme="info">都是</r-button>
+    <r-button theme="link">都是</r-button>
+  </div>
 </template>
 
 <script lang="ts">
