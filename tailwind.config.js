eace40857158fff5bad62627f15dc7fb7eedcffe
swanine <haohao1500@foxmail.com>
2024-05-30 22:03:06 +0800

    feature: rebirth

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{html,js}",
+        './src/**/*.{js,jsx,ts,tsx,vue}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
