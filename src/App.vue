2d2d1a03062a5812a37f0e7d0b3d635f972f1951
swanine <haohao1500@foxmail.com>
2024-09-03 21:18:42 +0800

    feature: add app

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { appWindow } from '@tauri-apps/api/window'
+document
+    .getElementById('titlebar-minimize')
+    .addEventListener('click', () => appWindow.minimize())
+document
+    .getElementById('titlebar-maximize')
+    .addEventListener('click', () => appWindow.toggleMaximize())
+document
+    .getElementById('titlebar-close')
+    .addEventListener('click', () => appWindow.close())
+// import Greet from "./components/Greet.vue";
+</script>
+
+<template>
+  <div class="w-full h-screen bg-white">
+    <div class="h-16 w-full px-10 border-b border-zinc-100"></div>
+
+
+
+<!--    <Greet />-->
+  </div>
+</template>
+
